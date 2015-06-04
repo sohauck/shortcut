9274a6eee6fa95a9b577ceae7b3c3b834f1b5823
sohauck <sofia.hauck@gmail.com>
2015-06-04 12:01:53 +0100

    added example files

--- lineage-assignment/separate-lineages.R
+++ lineage-assignment/separate-lineages.R
@@ -1,27 +1,33 @@
-library(cluster)
-library(reshape)
-library(ggplot2)
+library(cluster) # for creating matrix
+library(reshape) # for breaking down matrix into pairs
+library(ggplot2) # for plots
 
 #open file
-df <- read.csv(file.choose())
+in.file <- file.choose()
+df <- read.csv(in.file)
 
 # set up data frame with ids as row names and all variables as factors
 df <- data.frame(df[,-1], row.names=df[,1])
 df[] <- lapply(df, factor)
 
-# make dissimilarity matrix, including actual locus count (instead of proportion)
+# build dissimilarity matrix, including actual locus count (instead of proportion)
 dissmat <- ncol(df) * as.matrix(daisy(df, metric ="gower"))
 
-# write matrix to file
-write.csv(dissmat, file ='/Volumes/sofia/Mycobacterium/MTBC allelic diversity/temp-matrix.csv')
+# write matrix to file if wanted (so can avoid this lengthy step in future)
+# 
+# fileName1 <- file.choose(new=TRUE)
+# write.csv(dissmat, file =fileName1)
 
-# write pairwise to file
+# create pairwise table and write it to file
 pairs <- melt(dissmat)[melt(upper.tri(dissmat))$value,]
 names(pairs) <- c("c1", "c2", "distance")
 
-write.csv(pairs, file ='/Volumes/sofia/Mycobacterium/MTBC allelic diversity/temp-pairs.csv')
+out.file <- paste(gsub(".csv","", in.file),"_pairs.csv",sep="")
+write.csv(pairs, file=out.file)
 
 # plot to see cutoff
-ggplot(pairs, aes(distance)) + geom_histogram(binwidth=10) + geom_vline(xintercept=c(850), linetype="dotted")
+ggplot(pairs, aes(distance)) + 
+  geom_histogram(binwidth=10) + # change histogram to 
+  geom_vline(xintercept=c(850), linetype="dotted") # change intercept to choose cut-off
 
 # continue with separate-lineages-s2.pl
